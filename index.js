e07146308aa4985152b5ee2440ff6fd7d622971c
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-10-07 17:16:29 +0100

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * In this file you should define the following variables with the exact names
  *
  * 1. fullName      -> assign it a value of your full name
+ *
  * 2. yearOfBirth   -> assign it a value of your year of birth
  * 3. hobby         -> assign it a value of your favorite hobby
  * 4. funFact       -> assign it a value of some fun fact about yourself
@@ -11,10 +12,12 @@
 
 // Part 1 answer 👇🏻 ...
 
-
-
-
-
+const fullName = "fajri alhusaini";
+const yearOfBirthString = 1997;
+const hobby = "codding daily";
+const funFact = "i love pinnaple pizza ";
+const image =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Kuwait.svg/1200px-Flag_of_Kuwait.svg.png";
 
 /**
  * Part 2: String Interpolation
@@ -24,18 +27,13 @@
  * 1. fullNameString      -> assign it to: My name is {fullName}
  * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
  * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
- * 
+ *
  * Uncomment the following lines and start interpolating.
  * You can uncomment a line by removing the // at the start
  */
-
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
-
-
-
-
+const fullNameString = `My name is ${fullName}`;
+const YOUR_AGE = `I am ${2024 - yearOfBirthString}`;
+const hobbyString = `My hobby is ${hobby}`;
 /**
  * Part 3: Re-assignment
  * Increment your hacker score
@@ -45,14 +43,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars += 1;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars -= 1;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars += 2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars -= 2;
 }
